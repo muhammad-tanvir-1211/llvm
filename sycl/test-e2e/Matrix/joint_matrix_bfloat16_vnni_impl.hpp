6d976d4ea3c51d628c738be85d4fed73fe3de354
Muhammad Tanvir <muhammad.tanvir@codeplay.com>
2024-02-19 15:40:46 +0000

    Added implementation for joint_matrix for loading B matrix the usual way
    * Changed the input memory initialization to use the same matrices for both examples
    * Performance is upto 2X slower than using joint_matrix_load(B_packed)

--- sycl/test-e2e/Matrix/joint_matrix_bfloat16_vnni_impl.hpp
+++ sycl/test-e2e/Matrix/joint_matrix_bfloat16_vnni_impl.hpp
@@ -10,7 +10,7 @@
 template <size_t num_sub_groups, size_t frags_m, size_t frags_n, typename T1,
           typename T2, size_t M, size_t N, size_t K>
 void matrix_multiply(big_matrix<T1, M, N> &C, big_matrix<T2, M, K> &A,
-                     big_matrix<T2, K / 2, N * 2> &B) {
+                     big_matrix<T2, K, N> &B) {
   size_t frags_sg_m = M / TM;
   size_t frags_sg_n = N / TN;
   size_t sg_m = frags_sg_m / frags_m;
@@ -45,6 +45,8 @@ void matrix_multiply(big_matrix<T1, M, N> &C, big_matrix<T2, M, K> &A,
              sub_group sg = spmd_item.get_sub_group();
              const size_t sg_start_id =
                  spmd_item.get_global_linear_id() / SG_SZ;
+             const size_t sg_local_item_id =
+                 spmd_item.get_global_linear_id() % SG_SZ;
              const auto sg_starty = sg_start_id % sg_n;
              const auto sg_startx = sg_start_id / sg_n;
 
@@ -75,19 +77,33 @@ void matrix_multiply(big_matrix<T1, M, N> &C, big_matrix<T2, M, K> &A,
              }
 
              ptrA += (sg_startx * TM * frags_m) * K;
-             ptrB += (sg_starty * TN * 2 * frags_n);
+             ptrB += (sg_starty * TN * frags_n);
 #pragma unroll
              for (int k = 0; k < K / TK;
-                  k += 1, ptrA += TK, ptrB += (TK / 2) * (N * 2)) { //
+                  k += 1, ptrA += TK, ptrB += TK * N) { //
                auto newA = ptrA;
-               auto newB = ptrB;
+               auto newB = ptrB + sg_local_item_id;
 #pragma unroll
                for (int i = 0; i < frags_m; i++, newA += TM * K) {
                  joint_matrix_load(sg, sub_a[i], newA, K);
 #pragma unroll
-                 for (int j = 0; j < frags_n; j++, newB += TN * 2) {
+                 for (int j = 0; j < frags_n; j++, newB += TN) {
+                   T2 B_orig[TK];
                    if (i == 0) {
-                     joint_matrix_load(sg, sub_b[j], newB, N * 2);
+                     for (int kk = 0; kk < TK; kk++, newB += N) {
+                       B_orig[kk] = *newB;
+                     }
+
+                     using storage_element_type = typename sycl::ext::oneapi::
+                         detail::jm_type_interpretation_helper_trait<
+                             T2>::storage_element_type;
+                     auto wi_data_c =
+                         sycl::ext::oneapi::detail::get_wi_data(sg, sub_b[j]);
+                     for (int si = 0; si < wi_data_c.length(); si++) {
+                       storage_element_type element = wi_data_c[si];
+                       element = static_cast<storage_element_type>(B_orig[si]);
+                       wi_data_c[si] = element;
+                     }
                    }
                    joint_matrix_mad(sg, sub_c[i][j], sub_a[i], sub_b[j],
                                     sub_c[i][j]);
@@ -134,6 +150,8 @@ void matrix_multiply(big_matrix<T1, M, N> &C, big_matrix<T2, M, K> &A,
                sub_group sg = spmd_item.get_sub_group();
                const size_t sg_start_id =
                    spmd_item.get_global_linear_id() / SG_SZ;
+               const size_t sg_local_item_id =
+                   spmd_item.get_global_linear_id() % SG_SZ;
                const auto sg_starty = sg_start_id % sg_n;
                const auto sg_startx = sg_start_id / sg_n;
 
@@ -167,19 +185,34 @@ void matrix_multiply(big_matrix<T1, M, N> &C, big_matrix<T2, M, K> &A,
                }
 
                ptrA += (sg_startx * TM * frags_m) * K;
-               ptrB += (sg_starty * TN * 2 * frags_n);
+               ptrB += (sg_starty * TN * frags_n);
 #pragma unroll
                for (int k = 0; k < K / TK;
-                    k += 1, ptrA += TK, ptrB += (TK / 2) * (N * 2)) { //
+                    k += 1, ptrA += TK, ptrB += TK * N) { //
                  auto newA = ptrA;
                  auto newB = ptrB;
 #pragma unroll
                  for (int i = 0; i < frags_m; i++, newA += TM * K) {
                    joint_matrix_load(sg, sub_a[i], newA, K);
 #pragma unroll
-                   for (int j = 0; j < frags_n; j++, newB += TN * 2) {
+                   for (int j = 0; j < frags_n; j++, newB += TN) {
+                     T2 B_orig[TK];
                      if (i == 0) {
-                       joint_matrix_load(sg, sub_b[j], newB, N * 2);
+                       for (int kk = 0; kk < TK; kk++) {
+                         B_orig[kk] = *(newB + sg_local_item_id + kk * N);
+                       }
+
+                       using storage_element_type = typename sycl::ext::oneapi::
+                           detail::jm_type_interpretation_helper_trait<
+                               T2>::storage_element_type;
+                       auto wi_data_c =
+                           sycl::ext::oneapi::detail::get_wi_data(sg, sub_b[j]);
+                       for (int si = 0; si < wi_data_c.length(); si++) {
+                         storage_element_type element = wi_data_c[si];
+                         element =
+                             static_cast<storage_element_type>(B_orig[si]);
+                         wi_data_c[si] = element;
+                       }
                      }
                      joint_matrix_mad(sg, sub_c[i][j], sub_a[i], sub_b[j],
                                       sub_c[i][j]);
@@ -213,7 +246,6 @@ void matrix_multiply(big_matrix<T1, M, N> &C, big_matrix<T2, M, K> &A,
 template <size_t MATRIX_M, size_t MATRIX_N, size_t MATRIX_K> int run_gemm() {
   std::vector<bfloat16> A(MATRIX_M * MATRIX_K);
   std::vector<bfloat16> B(MATRIX_K * MATRIX_N);
-  std::vector<bfloat16> B_vnni((MATRIX_K / 2) * (MATRIX_N * 2));
   std::vector<float> C(MATRIX_M * MATRIX_N);
   std::vector<float> D(MATRIX_M * MATRIX_N);
 
@@ -222,18 +254,16 @@ template <size_t MATRIX_M, size_t MATRIX_N, size_t MATRIX_K> int run_gemm() {
   matrix_fill(MATRIX_M, MATRIX_N, C.data(), float{1});
   matrix_fill(MATRIX_M, MATRIX_N, D.data(), float{1});
 
-  matrix_vnni(MATRIX_K, MATRIX_N, B.data(), B_vnni.data(), 2);
-
   big_matrix<float, MATRIX_M, MATRIX_N> MC(C.data());
   big_matrix<float, MATRIX_M, MATRIX_N> MD(D.data());
   big_matrix<bfloat16, MATRIX_M, MATRIX_K> MA(A.data());
-  big_matrix<bfloat16, MATRIX_K / 2, MATRIX_N * 2> MB(B_vnni.data());
+  big_matrix<bfloat16, MATRIX_K, MATRIX_N> MB(B.data());
   printf("Running MatMul for M:%lu | N:%lu | K:%lu | NUM_SG:%d | FRAGS_M:%d | "
          "FRAGS_N:%d\n",
          MATRIX_M, MATRIX_N, MATRIX_K, NUM_SG, FRAG_M, FRAG_N);
   matrix_multiply<NUM_SG, FRAG_M, FRAG_N>(MC, MA, MB);
-  matrix_multiply_ref<bfloat16, bfloat16, float, 2>(
-      A.data(), B_vnni.data(), D.data(), MATRIX_M, MATRIX_N, MATRIX_K / 2);
+  matrix_multiply_ref<bfloat16, bfloat16, float, 1>(
+      A.data(), B.data(), D.data(), MATRIX_M, MATRIX_N, MATRIX_K);
 
   bool res = matrix_compare(MATRIX_M, MATRIX_N, C.data(), D.data());
   std::cout << (res ? "passed" : "failed") << std::endl;
